b3a50d11296bbe22fcd7ea461ec28dd5d6f402c7
binod2017 <binodk3@gmail.com>
2020-06-17 08:37:18 +0530

    initial commit

---
+++ build/precache-manifest.d90593c020b4183f3a8b82b9aeb27dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dbf38934da716f5e7b13f9b4b9131c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "055d245f6dae8e51f7de",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d037c0a18d30d1aa7a34",
+    "url": "/static/js/2.364c8765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.364c8765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055d245f6dae8e51f7de",
+    "url": "/static/js/main.298bc5d7.chunk.js"
+  },
+  {
+    "revision": "9ac12ee1cdb431a1326d",
+    "url": "/static/js/runtime-main.d57c06a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React JS June 16 2020</h1>
+        </header>
+        {/* <p className="App-intro">
+            To get Started, Edit <code>src/App.js</code> and save to reload.
+        </p>       */}
+      </div>
+    );
+  }
+}
+
+export default App;
